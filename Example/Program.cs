1168f17f67944fa7e90e2ab28cb4a994c7be321d
wei <li772555190@live.com>
2016-03-07 09:48:44 +0800

    build  vs2015

--- Example/Program.cs
+++ Example/Program.cs
@@ -1,121 +1,156 @@
 using System;
+using System.Diagnostics;
+using System.Net;
+using System.Security.Principal;
 using System.Threading;
 using WebSocketSharp;
 using WebSocketSharp.Net;
 
 namespace Example
 {
-  public class Program
-  {
-    public static void Main (string[] args)
-    {
-      // Create a new instance of the WebSocket class.
-      //
-      // The WebSocket class inherits the System.IDisposable interface, so you can
-      // use the using statement. And the WebSocket connection will be closed with
-      // close status 1001 (going away) when the control leaves the using block.
-      //
-      // If you would like to connect to the server with the secure connection,
-      // you should create the instance with the wss scheme WebSocket URL.
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			// Create a new instance of the WebSocket class.
+			//
+			// The WebSocket class inherits the System.IDisposable interface, so you can
+			// use the using statement. And the WebSocket connection will be closed with
+			// close status 1001 (going away) when the control leaves the using block.
+			//
+			// If you would like to connect to the server with the secure connection,
+			// you should create the instance with the wss scheme WebSocket URL.
 
-      using (var nf = new Notifier ())
-      using (var ws = new WebSocket ("ws://echo.websocket.org"))
-      //using (var ws = new WebSocket ("wss://echo.websocket.org"))
-      //using (var ws = new WebSocket ("ws://localhost:4649/Echo"))
-      //using (var ws = new WebSocket ("ws://localhost:4649/Echo?name=nobita"))
-      //using (var ws = new WebSocket ("wss://localhost:4649/Echo"))
-      //using (var ws = new WebSocket ("ws://localhost:4649/Chat"))
-      //using (var ws = new WebSocket ("ws://localhost:4649/Chat?name=nobita"))
-      //using (var ws = new WebSocket ("wss://localhost:4649/Chat"))
-      {
-        // Set the WebSocket events.
 
-        ws.OnOpen += (sender, e) => ws.Send ("Hi, there!");
+			//using (var ws = new WebSocket ("ws://echo.websocket.org"))
+			//using (var ws = new WebSocket ("wss://echo.websocket.org"))
+			//using (var ws = new WebSocket ("ws://localhost:4649/Echo"))
+			//using (var ws = new WebSocket ("ws://localhost:4649/Echo?name=nobita"))
+			//using (var ws = new WebSocket ("wss://localhost:4649/Echo"))
+			//using (var ws = new WebSocket ("ws://localhost:4649/Chat"))
+			//using (var ws = new WebSocket ("ws://localhost:4649/Chat?name=nobita"))
+			//using (var ws = new WebSocket ("wss://localhost:4649/Chat"))
+			using (var ws = new WebSocket("ws://bocorp-test.ioffice100.net/an/Users/bbd70056c684475e932b0dd6f90ffa50/CreateConnection?d=3/1/2016 8:11:18 AM"))
+			{
+				// Set the WebSocket events.
 
-        ws.OnMessage += (sender, e) =>
-          nf.Notify (
-            new NotificationMessage {
-              Summary = "WebSocket Message",
-              Body = !e.IsPing ? e.Data : "Received a ping.",
-              Icon = "notification-message-im"
-            });
+				ws.OnOpen += (sender, e) => ws.Send("Hi, there!");
 
-        ws.OnError += (sender, e) =>
-          nf.Notify (
-            new NotificationMessage {
-              Summary = "WebSocket Error",
-              Body = e.Message,
-              Icon = "notification-message-im"
-            });
+				ws.OnMessage += (sender, e) =>
+				{
+					Console.WriteLine(e.Data);
+				};
 
-        ws.OnClose += (sender, e) =>
-          nf.Notify (
-            new NotificationMessage {
-              Summary = String.Format ("WebSocket Close ({0})", e.Code),
-              Body = e.Reason,
-              Icon = "notification-message-im"
-            });
+				ws.OnError += (sender, e) =>
+				 {
+					 //Console.WriteLine(e.Data);
+				 };
+
+				ws.OnClose += (sender, e) =>
+			{
+				//	Console.WriteLine(e.Data);
+			};
 
 #if DEBUG
-        // To change the logging level.
-        ws.Log.Level = LogLevel.Trace;
+				// To change the logging level.
+				ws.Log.Level = LogLevel.Trace;
 
-        // To change the wait time for the response to the Ping or Close.
-        ws.WaitTime = TimeSpan.FromSeconds (10);
+				// To change the wait time for the response to the Ping or Close.
+				ws.WaitTime = TimeSpan.FromSeconds(10);
 
-        // To emit a WebSocket.OnMessage event when receives a ping.
-        ws.EmitOnPing = true;
+				// To emit a WebSocket.OnMessage event when receives a ping.
+				ws.EmitOnPing = true;
 #endif
-        // To enable the Per-message Compression extension.
-        //ws.Compression = CompressionMethod.Deflate;
+				// To enable the Per-message Compression extension.
+				//ws.Compression = CompressionMethod.Deflate;
 
-        /* To validate the server certificate.
-        ws.SslConfiguration.ServerCertificateValidationCallback =
-          (sender, certificate, chain, sslPolicyErrors) => {
-            ws.Log.Debug (
-              String.Format (
-                "Certificate:\n- Issuer: {0}\n- Subject: {1}",
-                certificate.Issuer,
-                certificate.Subject));
+				/* To validate the server certificate.
+				ws.SslConfiguration.ServerCertificateValidationCallback =
+				  (sender, certificate, chain, sslPolicyErrors) => {
+					ws.Log.Debug (
+					  String.Format (
+						"Certificate:\n- Issuer: {0}\n- Subject: {1}",
+						certificate.Issuer,
+						certificate.Subject));
 
-            return true; // If the server certificate is valid.
-          };
-         */
+					return true; // If the server certificate is valid.
+				  };
+				 */
 
-        // To send the credentials for the HTTP Authentication (Basic/Digest).
-        //ws.SetCredentials ("nobita", "password", false);
+				// To send the credentials for the HTTP Authentication (Basic/Digest).
+				//ws.SetCredentials ("nobita", "password", false);
 
-        // To send the Origin header.
-        //ws.Origin = "http://localhost:4649";
+				// To send the Origin header.
+				//ws.Origin = "http://localhost:4649";
 
-        // To send the Cookies.
-        //ws.SetCookie (new Cookie ("name", "nobita"));
-        //ws.SetCookie (new Cookie ("roles", "\"idiot, gunfighter\""));
+				// To send the Cookies.
+				//ws.SetCookie (new Cookie ("name", "nobita"));
+				//ws.SetCookie (new Cookie ("roles", "\"idiot, gunfighter\""));
 
-        // To connect through the HTTP Proxy server.
-        //ws.SetProxy ("http://localhost:3128", "nobita", "password");
+				//	To connect through the HTTP Proxy server.
 
-        // To enable the redirection.
-        //ws.EnableRedirection = true;
+				var proxy = HttpWebRequest.GetSystemWebProxy();
 
-        // Connect to the server.
-        ws.Connect ();
+				if (proxy != null)
+				{
+					var url = new Uri("http://bocorp-test.ioffice100.net/");
+					Uri proxyUri = proxy.GetProxy(url);
+					if (proxyUri != null)
+					{
+						if (proxyUri.Host != url.Host)
+						{
+							ICredentials credentials = CredentialCache.DefaultNetworkCredentials;
+							if (proxy.Credentials != null)
+							{
+								credentials = proxy.Credentials;
+							}
 
-        // Connect to the server asynchronously.
-        //ws.ConnectAsync ();
+							System.Net.NetworkCredential netCredential = null;
+							if (credentials != null)
+							{
+								//就是windows系统本地保存的用户凭证！这样基本没有什么技术问题了
+								netCredential = credentials.GetCredential(proxyUri, "Basic");
+								if (netCredential == null)
+								{
+									netCredential = credentials.GetCredential(proxyUri, "Digest");
+								}
+							}
 
-        Console.WriteLine ("\nType 'exit' to exit.\n");
-        while (true) {
-          Thread.Sleep (1000);
-          Console.Write ("> ");
-          var msg = Console.ReadLine ();
-          if (msg == "exit")
-            break;
+							var username = string.Empty;
+							var password = string.Empty;
 
-          // Send a text message.
-          ws.Send (msg);
-        }
-      }
-    }
-  }
+							if (netCredential != null)
+							{
+								username = netCredential.UserName;
+								password = netCredential.Password;
+							}
+							ws.SetProxy(proxyUri.AbsoluteUri, username, password);
+						}
+					}
+				}
+
+				// To enable the redirection.
+				//ws.EnableRedirection = true;
+
+				// Connect to the server.
+				ws.Connect();
+
+				// Connect to the server asynchronously.
+				//ws.ConnectAsync ();
+
+				Console.WriteLine("\nType 'exit' to exit.\n");
+				while (true)
+				{
+					Thread.Sleep(1000);
+					Console.Write("> ");
+					var msg = Console.ReadLine();
+					if (msg == "exit")
+						break;
+
+					// Send a text message.
+					ws.Send(msg);
+				}
+			}
+		}
+	}
 }
